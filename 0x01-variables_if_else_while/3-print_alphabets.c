fac2a2d11a2f8379792ca405cbca8034be0063d1
ROLITAL <sebagalaroland@gmail.com>
2022-09-12 17:02:40 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
+
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)*
  */
 int main(void)
 {
-	 char c = 'a';
-	  char C = 'A';
-	   while (c <= 'z')
-		    {
-			      putchar(c);
-			        c++;
-				 }
-	    while (C <= 'Z')
-		     {
-			       putchar(C);
-			         C++;
-				  }
-\
-	     putchar('\n');
-	      return (0);
+	char c = 'a';
+	char C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+
+	return (0);
 }
