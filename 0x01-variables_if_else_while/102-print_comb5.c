e3e0ea3c95abecafd5b030b7716a7b648df1b78d
ROLITAL <sebagalaroland@gmail.com>
2022-09-12 18:00:28 -0700

    last task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,5 @@
  #include <stdio.h>
+
 /**
  * main - main block
  * Description: Print all possible combinations of two two-digit numbers.
@@ -10,3 +11,40 @@
  * 00 01 and 01 00 are considered as the same combination.
  * You can only use putchar to print to consol
  * You can only use putchar up to 8 times.
+ * you are not allowed to use any variable of type 'char' .
+ * return: 0
+ */
+int main(void)
+{
+	int i, j;
+	int a, b, c, d;
+
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
+
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
+
+			if (a < c || (a == c && b < d))
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+		}
+	}
+}
+putchar(10);
+
+return (0);
+
