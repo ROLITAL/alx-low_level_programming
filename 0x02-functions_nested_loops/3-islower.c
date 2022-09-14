d672a8c84069f21ea76c44f2e0742ea2e3cd2a38
ROLITAL <sebagalaroland@gmail.com>
2022-09-14 13:27:58 -0700

    new

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,14 @@
 #include<stdio>
 
 /**
- * _islower i- checks whether or not a character is lowercase
- * @c: integer representing ascii value of character
- * Return: 1 if it is lowercase or 0 otherwise
+ *_islower i- checks whether or not a character is lowercase
+ *@c: integer representing ascii value of character 
+ *Return: 1 if it is lowercase or 0 otherwise
  */
 
-int _islower(int c) 
+int _islower(int c)
 {
-	if (c >= 97 && c <=  123)
-{
-	return (1);
-}
-	else
-{
+	if (c >= 97 && c <= 122)
+		return (1);
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * abs - function that computes the absolute
+ *
+ * takes integer type input
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
