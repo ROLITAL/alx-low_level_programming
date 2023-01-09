66962a5511eddb8298995faaf3cfc2442dc78d96
ROLITAL <sebagalaroland@gmail.com>
2023-01-09 11:50:59 -0800

    read me

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - A function that creats a hash table
+ * @sixe: the size of the array
+ *
+ * Return: if an error occurs  _NULL
+ *            OTHERWISE -a pointer to a new hash table
+ */
+
+hash_table_t 
